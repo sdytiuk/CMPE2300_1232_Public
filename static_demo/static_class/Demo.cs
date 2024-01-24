f8fac896efc1c8030ae056457dcfa910f8ec0fe4
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2024-01-24 09:03:28 -0700

    Demo complete

---
+++ static_demo/static_class/Demo.cs
@@ -0,0 +1,80 @@
+﻿/* comment block
+ * Steven Dytiuk
+ * This demos static and instance features of a class 
+ * in the context of busses that go places
+ * 2024-01-24 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/static-classes-and-static-class-members
+//the above talks about REAL static classes
+//THIS is not a real static class
+namespace static_class
+{
+    public class Demo
+    {
+        #region privatefields
+        private string _destination; //where the train is going
+        private string _carNo; //what is the car number
+        static string Something = "My static string"; //just to demo
+        public static Random random = null; //demoing that random should be static
+        #endregion
+        #region constructors
+        /// <summary>
+        /// Creates a new instance of demo
+        /// passing in the destination and carno
+        /// </summary>
+        /// <param name="dest">where the bus is going</param>
+        /// <param name="carNo">what is the car number</param>
+        public Demo(string dest, string carNo)
+        {
+            _destination = dest;
+            _carNo = carNo;
+            
+        }
+        /// <summary>
+        /// Static Constructor
+        /// PARAMETERLESS
+        /// Invoked when first needed
+        /// usually when an instance is created
+        /// But, could be when a static member is used
+        /// </summary>
+        static Demo()
+        {
+            Console.Write("Invoked Static Constructor");
+            TransitSystem = "ETS";
+            random = new Random();
+        }
+        #endregion
+
+        #region properties
+        public string destination
+        {
+            get { return _destination; }
+            private set { _destination = value; }
+        }
+        public string carNo
+        { get { return _carNo; } }
+
+        //automatic property:
+        public static string TransitSystem { get; private set; }
+        #endregion
+        #region methods
+        static public bool AreTheDemosTheSame(Demo demo1, Demo demo2)
+        {
+            return demo1.destination == demo2.destination &&
+                demo1.carNo == demo2.carNo;
+        }
+
+        public bool AmITheSameAs(Demo another)
+        {
+            return AreTheDemosTheSame(this, another);
+            
+        }
+        #endregion
+    }
+}
---
+++ static_demo/static_demo/Program.cs
@@ -0,0 +1,53 @@
+﻿/* TODO:
+ * * commenting standards
+ * comment block on each file
+ * * type library
+ * * static demo
+ * Steven Dytiuk
+ * 2024-01-24
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using static_class;
+
+namespace static_demo
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sydney's Notes: " + Demo.TransitSystem);
+            Demo colesBus = new Demo("St. Albert Centre", "StAT01");
+            Thread.Sleep(1000);
+            Console.WriteLine("Cole's Notes: " + Demo.TransitSystem);
+            Demo griffinBus = new Demo("Mill Woods TC", "123");
+            Demo akilBus = new Demo("Honolulu", "Ford F150");
+            Demo sydneyBus = new Demo("Honolulu", "Ford F150");
+
+            //we are displaying multiple randoms to test it
+            //
+            Console.WriteLine(Demo.random.Next());
+            Console.WriteLine(Demo.random.Next());
+            Console.WriteLine(Demo.random.Next());
+            Console.WriteLine(Demo.random.Next());
+
+            //showing the difference between static method and instance method
+            Console.WriteLine(Demo.AreTheDemosTheSame(akilBus,sydneyBus));
+            Console.WriteLine(Demo.AreTheDemosTheSame(griffinBus, sydneyBus));
+            Console.WriteLine(colesBus.AmITheSameAs( sydneyBus));
+
+            Console.ReadLine();
+        }
+    }
+}
