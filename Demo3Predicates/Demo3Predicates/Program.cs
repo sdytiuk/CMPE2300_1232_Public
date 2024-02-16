513484864152637f9e4e2f68f757d8a3c87956c8
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2024-02-16 10:12:26 -0700

    Predicates (non) demo

---
+++ Demo3Predicates/Demo3Predicates/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3Predicates
+{
+    internal class Program
+    {
+        public static bool isBigger(int iVal)
+        {
+            return iVal > 5;
+        }
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Random rand = new Random();
+            for (int i = 0; i< 10; i++)
+            {
+                list.Add(rand.Next(0,10));
+            }
+
+            foreach (int i in list)
+            {
+                Console.Write(i + ", ");
+            }
+
+            Console.WriteLine(list.Find(new Predicate<int>(isBigger)));
+
+            Console.WriteLine(list.Find(isBigger));
+
+            Console.ReadKey();
+        }
+    }
+}
