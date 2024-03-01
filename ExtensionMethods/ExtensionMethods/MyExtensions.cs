98224f5752050751b2928293734d9781f74fdb04
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2024-03-01 09:54:28 -0700

    Custom Extension Demo

---
+++ ExtensionMethods/ExtensionMethods/MyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        //a custom extension method is always decorated with this
+        public static string toFixed2(this double dVal)
+        {
+            return dVal.ToString("F2");
+        }
+
+        public static string ColesNotes(this string sVal)
+        {
+            return "Cole's " + sVal + " is always number 1!";
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class ShaneQuestion
+    {
+        public static string toFixed3(this double dVal)
+        {
+            return dVal.ToString("F3");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //double value that you want to display as fixed 2 decimal places
+            double x = 3.14159;
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine(x.toFixed2());
+            Console.WriteLine(x.toFixed3());
+            string cole = "question";
+            Console.WriteLine(cole);
+            Console.WriteLine(cole.ColesNotes());
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
