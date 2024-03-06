8ccebd024ee538bf51483fafc8cee6f424849a11
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2024-03-06 09:00:48 -0700

    More extension methods

--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -19,6 +19,7 @@ namespace ExtensionMethods
     {
         static void Main(string[] args)
         {
+            /*
             //double value that you want to display as fixed 2 decimal places
             double x = 3.14159;
             Console.WriteLine(x.ToString("F2"));
@@ -27,7 +28,50 @@ namespace ExtensionMethods
             string cole = "question";
             Console.WriteLine(cole);
             Console.WriteLine(cole.ColesNotes());
+            */
+            //using extension methods
+            List<int> aList = new List<int>();
+            List<int> bList = new List<int>();
+            Random rnd = new Random();
 
+            for (int i = 0; i < 10; i++)
+            {
+                aList.Add(rnd.Next(10));
+                bList.Add(rnd.Next(10));
+            }
+
+            Console.WriteLine("Both lists:");
+            aList.ForEach(a => Console.Write(a.ToString()+","));
+            Console.WriteLine();
+            bList.ForEach(b => Console.Write(b.ToString() + ","));
+            Console.WriteLine($"ASum = {aList.Sum()}") ;
+            Console.WriteLine($"BSum = {bList.Sum()}");
+            Console.WriteLine($"CombinedSum = {aList.Concat(bList).Sum()}");
+            //how to use indexof and insert
+            int num = 555;
+            int where;
+            where = aList.IndexOf(num); //returns -1 if number not found
+            int numtoinsert = 99;
+            if (where >= 0)
+                aList.Insert(where, numtoinsert);
+            aList.ForEach(a => Console.Write(a.ToString() + ","));
+            Console.WriteLine();
+            //filter out certain values (less than 3)
+            IEnumerable<int> e = aList.Where(x => x < 3);
+            e.ToList().ForEach(a => Console.Write(a.ToString() + ","));
+            foreach (int i in e)
+            { 
+                Console.Write(i.ToString()); 
+            }
+            //aList.ForEach(a => Console.Write(a.ToString() + ",")); Note: AList WONT BE CHANGED
+
+            //find common numbers
+            Console.WriteLine();
+            IEnumerable<int> common = aList.Intersect(bList);
+            foreach (int i in common)
+            {
+                Console.Write(i.ToString()+",");
+            }
             Console.ReadKey();
         }
 
