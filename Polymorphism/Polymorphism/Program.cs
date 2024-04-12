cfc4ff5438d6f79c6d40728c5ef9398d2b3f3e60
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2024-04-12 08:35:03 -0600

    basic demo

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<cBase> list = new List<cBase>();
+            list.Add(new cBase());
+            list.Add(new CDerived());
+
+            list.ForEach(x => x.Who());
+
+            Console.ReadLine();
+        }
+    }
+
+    class cBase
+    {
+        public virtual void Who()
+        {
+            Console.WriteLine("Who in base!");
+        }
+    }
+
+    class CDerived : cBase
+    {
+        public override void Who()
+        {
+            Console.WriteLine("Who in CDerived");
+        }
+    }
+}
