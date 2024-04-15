569c09a8fb12d5f86aeb9a8993f69e297033d6ae
Steven Dytiuk <84546026+sdytiuk@users.noreply.github.com>
2024-04-15 14:59:33 -0600

    demo complete

---
+++ nvidemo/nvidemo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nvidemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DerLogger x = new DerLogger();
+            x.Log("Cole is confused now.");
+
+            ColeLager y = new ColeLager();
+            y.Log("Cole is refreshed now.");
+
+            Console.ReadLine();
+        }
+
+        /* Abstract Class
+         * can't have an instance
+         * Purpose is to derive from it
+         * */
+        abstract class Logger
+        {
+            public void Log(string message) 
+            { 
+                Console.WriteLine(DateTime.Now.ToShortTimeString());
+                if (this is ColeLager)
+                {
+                    Console.WriteLine("Wow, Cole comes up with strange ideas!");
+                }
+                VLog(message);
+                Console.WriteLine("Done");
+            }
+
+            protected abstract void VLog(string message);
+
+
+        }
+
+        class DerLogger : Logger
+        {
+            protected override void VLog(string message)
+            {
+                Console.WriteLine($"DerLogger : {message}");
+            }
+        }
+
+        class ColeLager : Logger
+        {
+            protected override void VLog(string message)
+            {
+                Console.WriteLine($"ColeLager : {message}");
+            }
+        }
+    }
+}
---
+++ nvidemo/nvidemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("nvidemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NAIT")]
+[assembly: AssemblyProduct("nvidemo")]
+[assembly: AssemblyCopyright("Copyright © NAIT 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ff54bc6d-9ec6-45b0-b3df-77d915d0526d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
